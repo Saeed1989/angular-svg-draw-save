36bba2c014e67ef23cc9f9270ca124d1c1d26fd8
saeed1 <saeed.sharman@gmail.com>
2022-05-19 17:08:19 +0900

    makes json read write controller

--- svg-draw-server/SvgDrawServer/Controllers/RectangleController.cs
+++ svg-draw-server/SvgDrawServer/Controllers/RectangleController.cs
@@ -6,78 +6,35 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SvgDrawServer.Models;
+using System.Text.Json;
 
 namespace SvgDrawServer.Controllers
 {
+
     [Route("api/[controller]")]
     [ApiController]
     public class RectangleController : ControllerBase
     {
-        private readonly RectangleContext _context;
 
-        public RectangleController(RectangleContext context)
+        private readonly JsonSerializerOptions options = new JsonSerializerOptions
         {
-            _context = context;
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true
+        };
+
+        public RectangleController()
+        {
         }
 
         // GET: api/Rectangle
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Rectangle>>> GetRectangleOption()
         {
-          if (_context.RectangleOption == null)
-          {
-              return NotFound();
-          }
-            return await _context.RectangleOption.ToListAsync();
-        }
-
-        // GET: api/Rectangle/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Rectangle>> GetRectangle(long id)
-        {
-          if (_context.RectangleOption == null)
-          {
-              return NotFound();
-          }
-            var rectangle = await _context.RectangleOption.FindAsync(id);
-
-            if (rectangle == null)
-            {
-                return NotFound();
-            }
-
-            return rectangle;
-        }
-
-        // PUT: api/Rectangle/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutRectangle(long id, Rectangle rectangle)
-        {
-            if (id != rectangle.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(rectangle).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!RectangleExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
+            var jsonString = System.IO.File.ReadAllText("Data/rectangle-data.json");
+            var jsonModel = JsonSerializer.Deserialize<Rectangle>(jsonString, options);
+            if (jsonModel != null)
+                return Ok(jsonModel);
+            else return NotFound();
         }
 
         // POST: api/Rectangle
@@ -85,40 +42,9 @@ namespace SvgDrawServer.Controllers
         [HttpPost]
         public async Task<ActionResult<Rectangle>> PostRectangle(Rectangle rectangle)
         {
-          if (_context.RectangleOption == null)
-          {
-              return Problem("Entity set 'RectangleContext.RectangleOption'  is null.");
-          }
-            _context.RectangleOption.Add(rectangle);
-            await _context.SaveChangesAsync();
-
-            //return CreatedAtAction("GetRectangle", new { id = rectangle.Id }, rectangle);
+            System.IO.File.WriteAllText("Data/rectangle-data.json", JsonSerializer.Serialize(rectangle, options));
             return CreatedAtAction(nameof(GetRectangleOption), new { id = rectangle.Id }, rectangle);
         }
 
-        // DELETE: api/Rectangle/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteRectangle(long id)
-        {
-            if (_context.RectangleOption == null)
-            {
-                return NotFound();
-            }
-            var rectangle = await _context.RectangleOption.FindAsync(id);
-            if (rectangle == null)
-            {
-                return NotFound();
-            }
-
-            _context.RectangleOption.Remove(rectangle);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool RectangleExists(long id)
-        {
-            return (_context.RectangleOption?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
     }
 }
--- svg-draw-server/SvgDrawServer/Program.cs
+++ svg-draw-server/SvgDrawServer/Program.cs
@@ -1,28 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using SvgDrawServer.Models;
-
-
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-builder.Services.AddDbContext<RectangleContext>(opt =>
-    opt.UseInMemoryDatabase("RectanlgleOption"));
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new() { Title = "ShapeApi", Version = "v1" });
-//});
-
+  
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (builder.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    //app.UseSwagger();
-    //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
 }
 
 app.UseHttpsRedirection();
