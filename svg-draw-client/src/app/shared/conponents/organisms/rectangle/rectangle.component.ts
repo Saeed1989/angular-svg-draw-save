d5d358ef3c96c4f42fb6f73fb89b1ca5d13a2b7e
saeed1 <saeed.sharman@gmail.com>
2022-05-20 11:58:23 +0900

    adds dragging and resizing

--- svg-draw-client/src/app/shared/conponents/organisms/rectangle/rectangle.component.ts
+++ svg-draw-client/src/app/shared/conponents/organisms/rectangle/rectangle.component.ts
@@ -7,11 +7,87 @@ import { RecShapeOptions } from 'src/app/core/models/rec-shape-options.model';
   styleUrls: ['./rectangle.component.css'],
 })
 export class RectangleComponent implements OnInit {
+  @Input() shapeOptions: RecShapeOptions;
+  @Output() shapeOptionsChange = new EventEmitter<RecShapeOptions>();
 
- @Input() shapeOptions: RecShapeOptions;
- @Output() shapeOptionsChange = new EventEmitter<RecShapeOptions>();
+  isDragging: boolean = false;
+  isDrawing: boolean = false;
+  isResizing: boolean = false;
+  isSelectingPoints: boolean = false;
+
+  resizeStartX = 0;
+  resizeStartY = 0;
+
+  draggingStartX = 0;
+  draggingStartY = 0;
+
+  svg: any;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.svg = document.querySelector('svg');
+  }
+
+  onMouseDown(event: any): void {
+    //this.getMousePosition(event);
+    if (event.target.classList.contains('draggable')) {
+      this.startDragging(event);
+    } else if (event.target.classList.contains('resizable')) {
+      this.startResizing(event);
+    }
+  }
+
+  onMouseMove(event: any): void {
+    if (this.isDragging) {
+      this.dragToMousePosition(event);
+    } else if (this.isResizing) {
+      this.resizeToMousePosition(event);
+    }
+  }
+
+  onMouseUp(event: any): void {
+    if (this.isDragging) {
+      this.dragToMousePosition(event);
+    } else if (this.isResizing) {
+      this.resizeToMousePosition(event);
+    }
+    this.isDrawing = false;
+    this.isDragging = false;
+    this.isResizing = false;
+  }
+
+  startDragging(event: any): void {
+    this.isDragging = true;
+    this.draggingStartX = event.clientX;
+    this.draggingStartY = event.clientY;
+    console.log('startDragging()');
+  }
+
+  startResizing(event: any): void {
+    this.isResizing = true;
+    this.resizeStartX = event.clientX;
+    this.resizeStartY = event.clientY;
+    console.log('startResizing()');
+  }
+
+  dragToMousePosition(event: MouseEvent) {
+    const dragX = event.clientX - this.draggingStartX;
+    const dragY = event.clientY - this.draggingStartY;
+    this.draggingStartX = event.clientX;
+    this.draggingStartY = event.clientY;
+    this.shapeOptions.originX += dragX;
+    this.shapeOptions.originY += dragY;
+  }
+
+  resizeToMousePosition(event: MouseEvent) {
+    const dragX = event.clientX - this.resizeStartX;
+    const dragY = event.clientY - this.resizeStartY;
+    this.resizeStartX = event.clientX;
+    this.resizeStartY = event.clientY;
+    this.shapeOptions.width += dragX;
+    this.shapeOptions.height += dragY;
+  }
+
+  getSvg() {}
 }
